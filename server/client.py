7d9b44e0ff3217faf103b803777444d96a00ae60
= <=>
2020-06-24 17:55:01 +0300

    add async state processing

--- server/client.py
+++ server/client.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 
 import paho.mqtt.client as mqtt
-from omxplayer.player import OMXPlayer
+# from omxplayer.player import OMXPlayer
 from pathlib import Path
 
 
@@ -12,29 +12,120 @@ VIDEO_PATH = Path('/home/pi/tech_video_comp0001-0630.mp4')
 
 class Machine:
 
-    state_idle = 'IDLE'
-    state_opening = 'OPENING'
-    state_hold = 'HOLD ON'
-    state_qr = 'QR CODE'
+   # state_idle = 'IDLE'
+   # state_opening = 'OPENING'
+   # state_cycle_content = 'CYCLE CONTENT'
+   # state_finish_content = 'FINISH CONTENT'
+   # state_hold = 'HOLD ON'
+   # state_return = 'RETURN'
+   # state_qr = 'QR CODE'
+
+    
 
     def __init__(self):
+        # self.player = OMXPlayer(VIDEO_PATH, args=['--blank', '--loop'])
+
+        self.state_idle = self.idle
+        self.state_opening = self.opening
+        self.state_cycle_content = self.cycle_content
+        self.state_hold_on = self.hold_on
+        self.state_finish_content = self.finish_content
+        self.state_return = self.show_return
+        self.state_qr = self.show_qr
+
         self.state = self.state_idle
-        self.player = OMXPlayer(VIDEO_PATH, args=['--blank', '--loop'])
+
+    async def idle(self):
+        print('idling')
+        #await asyncio.sleep(1)
+
+    async def opening(self):
+        print('opening')
+        #await asyncio.sleep(1)
+        self.state = self.state_cycle_content
+
+    async def cycle_content(self):
+        print('cycling')
+        await asyncio.sleep(1)
+
+    async def hold_on(self):
+        print('hold on')
+        #await asyncio.sleep(1)
+        self.state = self.state_finish_content 
+    
+    async def finish_content(self):
+        print('finishing content')
+        #await asyncio.sleep(1)
+        self.state = self.state_return
+
+    async def show_return(self):
+        print('returning')
+        #await asyncio.sleep(1)
+        self.state = self.state_cycle_content
+
+    async def show_qr(self):
+        print('showing qr')
+        #await asyncio.sleep(1)
+        self.state = self.state_cycle_content
+
+
+machine = Machine()
+
+async def process_state():
+    while True:
+        await machine.state()
+        await(asyncio.sleep(1))
+
+
+### MQTT STUFF ###
 
 def on_message(mosq, obj, msg):
     print(msg.topic + ' ' + str(msg.qos) + ' ' + str(msg.payload))
 
-def idle(mosq, obj, msg):
-    print(msg.topic + ' ' + str(msg.qos) + ' ' + str(msg.payload))
-    machine.state = machine.state_idle
-    machine.process_state()
+def parse_message(mosq, obj, msg):
+    print('parsing_message: ', msg.topic, msg.payload)
+    
+    stand, thing, sensor = msg.topic.split('/')
+
+    # machine.state = machine.state_opening
+
+    if sensor == 'proximity':
+        if msg.payload == b'True':  # person came
+            if machine.state == machine.state_idle:
+                machine.state = machine.state_opening
+            elif machine.state in [machine.state_cycle_content, machine.state_qr]:
+                machine.state = machine.state_hold_on
+        else:  # person left
+            if machine.state == machine.state_cycle_content:
+                machine.state = machine.state_idle
+    elif sensor == 'interaction':
+        if msg.payload == b'True':  # thing taken
+            if machine.state in [machine.state_cycle_content, machine.state_qr]: 
+                machine.state = machine.state_hold_on
+            else:
+                machine.state = machine.state_qr
+        else:  # thing put down
+            machine.state = machine.state_cycle_content
+
 
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
 mqttc.connect(HOST, 1883, 60)
 mqttc.subscribe('#', 0)
 
-machine = Machine()
+mqttc.message_callback_add("stand0/#", parse_message)
 
-mqttc.loop_forever()
+
+async def read_messages():
+    while True:
+        mqttc.loop()
+        await asyncio.sleep(0.1)
+
+
+loop = asyncio.get_event_loop()
+task = loop.create_task(read_messages())
+task2 = loop.create_task(process_state())
+
+
+loop.run_forever()
 
--- server/mqtt_emu.py
+++ server/mqtt_emu.py
@@ -4,23 +4,37 @@ import itertools
 import random
 import time
 
+import argparse
+
 import paho.mqtt.publish as publish
 
 
-HOSTNAME = '127.0.0.1' 
+HOSTNAME = '10.0.2.17' 
+HOSTNAME = '127.0.0.1'
 
 POSSIBLE_STATES = [
     (
    'stand{}/thing{}/{}'.format(s, t, d), v) for s, t, d, v in itertools.product([0], range(4), ('proximity', 'interaction'), (True, False))  
    ]
 
-print(POSSIBLE_STATES)
+# print(POSSIBLE_STATES)
 
 
-while True:
+parser = argparse.ArgumentParser(description='arguments')
+parser.add_argument('--random', action='store_true', dest='random')
+parser.add_argument('-t', action='store', dest='topic')
+parser.add_argument('-v', action='store', dest='value')
 
-    message = random.choice(POSSIBLE_STATES)
+
+args = parser.parse_args()
+
+if __name__ == '__main__':   
+
+    if args.random: 
+        message = random.choice(POSSIBLE_STATES)
+        # time.sleep(random.random())  
+    else:
+        message = (args.topic, args.value) 
+
     print(message, '->', HOSTNAME)
     publish.single(message[0], message[1], hostname=HOSTNAME)
-    time.sleep(random.random())  
-
